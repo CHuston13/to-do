98026e470538ec3336b13fd7b1aad27f452e6bc8
Chuston13 <cdhprogrammer@gmail.com>
2022-09-01 03:06:35 -0500

    index.html

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import _ from 'lodash';
+
+import './style.css';
+import Icon from './paper.jpg'
+
+
+ function createBackground() {
+ const element = document.createElement('div');
+ const div2 = document.createElement('div');
+ const addButton = document.createElement('button');
+ const myIcon = new Image();
+myIcon.src = Icon;
+ 
+ 
+  
+
+
+
+
+
+
+//appending
+
+element.appendChild(div2);
+
+element.appendChild(myIcon);
+div2.appendChild(addButton);
+
+
+
+//styling
+element.classList.add('background');
+div2.classList.add('innerSection');
+   
+  
+  
+  
+  
+  
+  
+  
+  
+  return element;
+
+
+
+
+ 
+ }
+
+ document.body.appendChild(createBackground());
