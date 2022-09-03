63595397376bab527263e01ac6189cc4d2de0651
Chuston13 <cdhprogrammer@gmail.com>
2022-09-03 09:18:30 -0500

    dist/index.bundle.js

--- src/secondary.js
+++ src/secondary.js
@@ -1,10 +1,36 @@
+
+
+
+
 export default function createPrompt(){
+    const div3 = document.createElement('div');
     const input = document.createElement('input');
     input.setAttribute('type','text');
+    const date = document.createElement('input');
+    date.setAttribute('type','date');
     const pull = document.getElementById('second-born');
-    pull.appendChild(input);
-    
+    const newBut = document.createElement('button');
+    newBut.innerHTML='submit';
+    newBut.onclick=submitButton;
 
-    return input
+
+
+
+
+    //appending
+    pull.appendChild(div3);
+    div3.appendChild(input);
+    div3.appendChild(date);
+    div3.appendChild(newBut);
+
+    return input, date, newBut;
    }
+
+
+   function submitButton(){
+    const remove = document.getElementById('second-born');
+    remove.removeAttribute;
+   }
+
+
    
